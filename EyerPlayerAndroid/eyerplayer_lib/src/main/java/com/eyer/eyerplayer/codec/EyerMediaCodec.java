107198064258685aa06b84da0d3164acc5c59f82
yuqiaomiao <yuqiaomiao@sohu-inc.com>
2020-08-13 18:38:22 +0800

    mediacodec

--- EyerPlayerAndroid/eyerplayer_lib/src/main/java/com/eyer/eyerplayer/codec/EyerMediaCodec.java
+++ EyerPlayerAndroid/eyerplayer_lib/src/main/java/com/eyer/eyerplayer/codec/EyerMediaCodec.java
@@ -1,9 +1,14 @@
 package com.eyer.eyerplayer.codec;
 
 import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaCodecList;
 import android.media.MediaFormat;
+import android.os.Build;
 import android.util.Log;
 import android.view.Surface;
+
+import androidx.annotation.RequiresApi;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -13,6 +18,7 @@ public class EyerMediaCodec {
     private MediaCodec mediaCodec = null;
 
     public int init(int width, int height){
+        displayDecoders();
         Log.e("EyerMediaCodec", "width: " + width + " height: " + height);
 
         try {
@@ -39,12 +45,32 @@ public class EyerMediaCodec {
         return 0;
     }
 
+    private void displayDecoders() {
+        MediaCodecList list = null;//REGULAR_CODECS参考api说明
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            list = new MediaCodecList(MediaCodecList.REGULAR_CODECS);
+        }
+        MediaCodecInfo[] codecs = new MediaCodecInfo[0];
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            codecs = list.getCodecInfos();
+        }
+        if(list != null) {
+            for (MediaCodecInfo codec : codecs) {
+                if (codec.isEncoder())
+                    continue;
+                Log.e("EyerMediaCodec", "Decoder Name: " + codec.getName());
+            }
+        }
+    }
+
     public int input(){
+        /*
         int inputBufIndex = mediaCodec.dequeueInputBuffer(10000);
         if (inputBufIndex >= 0) {
             ByteBuffer inputBuf = mediaCodec.getInputBuffers()[inputBufIndex];
             mediaCodec.queueInputBuffer(inputBufIndex, 0, chunkSize, mMediaExtractor.getSampleTime(), 0);
         }
+        */
 
         return 0;
     }
