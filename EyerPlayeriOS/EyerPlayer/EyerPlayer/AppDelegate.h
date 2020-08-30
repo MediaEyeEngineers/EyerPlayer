04a8c157d1a773aeacd71d8008859d0f1854824a
Redknot <redknotmiaoyuqiao@gmail.com>
2020-08-31 04:03:24 +0800

    iOS project

---
+++ EyerPlayeriOS/EyerPlayer/EyerPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EyerPlayer
+//
+//  Created by Yuqiao Miao on 2020/8/31.
+//  Copyright © 2020 Yuqiao Miao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ EyerPlayeriOS/EyerPlayer/EyerPlayer/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  EyerPlayer
+//
+//  Created by Yuqiao Miao on 2020/8/31.
+//  Copyright © 2020 Yuqiao Miao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
